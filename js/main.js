380cc6ff451df661b2da5332bf3227fdf0d37d87
hernanfernandez <hernanenriquefernandez2019@gmail.com>
2025-06-05 20:42:37 -0300

    se trabajo en el responsive

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerar");
+
+
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click",() => {
+    nav.classList.remove("visible");
+})
+
+
+
+
+
+
