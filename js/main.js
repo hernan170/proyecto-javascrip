fd44820cdb07bca136984dcad7b31213c2d58067
hernanfernandez <hernanenriquefernandez2019@gmail.com>
2025-06-06 21:55:25 -0300

    se modofico imagen y header

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ abrir.addEventListener("click", () => {
     nav.classList.add("visible");
 })
 
-cerrar.addEventListener("click",() => {
+cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
 
